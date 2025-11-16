4c7d2e1ae604322d08ee74a4defceea7b48645f9
marioandreibobi <bobimarioandrei@gmail.com>
2025-11-16 22:17:52 +0200

    Validtion function for Exit and Drop table

--- ILoveBlondes/ValidaitionFunctions.cpp
+++ ILoveBlondes/ValidaitionFunctions.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 string Trim(string s) {
+	while (!s.empty() && isspace(s.front()))
+		s.erase(s.begin());
 
-	while (!s.empty() && s.front() == ' ') s.erase(s.begin());
-	while (!s.empty() && s.back() == ' ')s.pop_back();
+	while (!s.empty() && isspace(s.back()))
+		s.pop_back();
+
 	return s;
 }
+
 
 bool validateCreateTable(string input, CommandType type) {
 	string cmd = UpperCaseTransf(input); //for case insensitive
@@ -93,4 +97,58 @@ bool validateCreateTable(string input, CommandType type) {
 
 
 	return numColumns > 0;
+}
+
+bool validateDropTable(string input, CommandType type) {
+	string cmd = UpperCaseTransf(input);
+	
+	//check where the string is
+	int start = 0;
+	while (start < cmd.length() &&
+		(cmd[start] == ' ' || cmd[start] == '\t' || cmd[start] == '\n')) {
+		start++;
+	}
+	//if empty delete
+	if (start == cmd.length())return false;
+
+
+	cmd = cmd.substr(start);
+	//check if it is CREATE TABLE
+	if (cmd.rfind("DROP TABLE", 0) != 0) {
+		return false;
+	}
+
+	int pos = 10; //start of the table name
+	string tableName = Trim(cmd.substr(pos));
+
+	if (!tableName.empty()) return false;
+	
+	if (!isalpha(tableName[0])) return false;
+
+
+	for (int i = 0; i < tableName.length(); i++) {
+		char c = tableName[i];
+		if (!isalnum(c) && c != '_') return false;
+	}
+
+
+	return true;
+}
+
+bool ValidateExit(string s, CommandType type) {
+	string cmd = UpperCaseTransf(s);
+
+	cmd = Trim(cmd);
+
+	//check if it is EXIT
+	if (cmd.rfind("EXIT", 0) != 0) {
+		return false;
+	}
+
+	string rest = cmd.substr(4);
+
+	if (Trim(rest) != "")
+		return false;
+
+	return true;
 }
