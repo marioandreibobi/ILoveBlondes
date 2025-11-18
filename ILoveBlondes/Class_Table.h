a19f1a59430b97e04565a71785b21387afdb94bb
Gigel NUMERO 1 <bobimarioandrei@gmail.com>
2025-11-18 15:53:42 +0200

    Class Table

---
+++ ILoveBlondes/Class_Table.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"Class_Column.h"
+#include <iostream>
+
+using namespace std;
+
+class Table {
+private:
+	string name = "";
+	Column* columns = nullptr;
+	int noColoumns = 0;
+public:
+};
